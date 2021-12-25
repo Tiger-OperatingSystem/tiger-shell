003f00e91319489ec68d2e1d829080b34ab58700
Natanael <natanaelgplay@gmail.com>
2021-12-25 18:09:05 -0300

    Update Kalendario.sh

--- scripts/Kalendario.sh
+++ scripts/Kalendario.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 [ "${1}" = "--display-dialog" ] && {
   choose_date=$(yad --calendar --mouse --undecorated --close-on-unfocus --skip-taskbar --width=480 --heigth=380 --borders=32 --button="Abrir no Google Agenda":0 --date-format='%Y/%m/%d')
 
@@ -12,8 +13,9 @@
 
 SELF=$(readlink -f "${0}")
 
-
-time=$(date '+%H:%M%n %d/%m/%Y ')
+# O generic-monitor do XFCE da LTS 20.04 do Ubuntu não centraliza automaticamente o texto
+# isso foi corrigido no 22.04 mas ainda não foi lançado
+time=$(date '+   %H:%M%n %d/%m/%Y ')
 
 tooltip=$(date '+Hoje é %d de %B de %Y')
 
