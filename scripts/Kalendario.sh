3407f6d41091922cfcaf72695bd33bd6d70548b3
Natanael <natanaelgplay@gmail.com>
2021-12-26 11:01:13 -0300

    Update Kalendario.sh

--- scripts/Kalendario.sh
+++ scripts/Kalendario.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Calendário do Tiger OS, ele substitui o calendário padrão do XFCE através do monitor generico
+
 
 [ "${1}" = "--display-dialog" ] && {
   choose_date=$(yad --calendar --mouse --undecorated --close-on-unfocus --skip-taskbar --width=480 --heigth=380 --borders=32 --button="Abrir no Google Agenda":0 --date-format='%Y/%m/%d')
@@ -15,7 +17,8 @@ SELF=$(readlink -f "${0}")
 
 # O generic-monitor do XFCE da LTS 20.04 do Ubuntu não centraliza automaticamente o texto
 # isso foi corrigido no 22.04 mas ainda não foi lançado
-time=$(date '+   %H:%M%n %d/%m/%Y ')
+# time=$(date '+   %H:%M%n %d/%m/%Y ')
+time=$(date '+%H:%M%n %d/%m/%Y ')
 
 tooltip=$(date '+Hoje é %d de %B de %Y')
 
