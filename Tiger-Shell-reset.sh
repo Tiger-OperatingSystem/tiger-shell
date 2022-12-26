d4069f3ca854d72b256bdadb271f7d34d410dc9d
Natanael <natanaelgplay@gmail.com>
2022-12-26 15:58:19 -0300

    Update Tiger-Shell-reset.sh

--- Tiger-Shell-reset.sh
+++ Tiger-Shell-reset.sh
@@ -4,3 +4,5 @@ yad --borders=32 --width=640 --button="Não, cancele":1 --button="Sim, resete a 
     --text="<big><b>Como deseja resetar o Tiger Shell?</b></big>\nNote que essa ação <b>NÃO</b> pode ser desfeita, seus arquivos não serão afetados\n"  && { 
         tiger-shell.sh --refresh;
 }
+
+xfce4-settings-manager
