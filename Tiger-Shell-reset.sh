4c8069f4006124b7ec046f3bbe3e189c7c27e05c
Daigo <openwebosbr@gmail.com>
2022-12-27 05:53:04 -0300

    Update Tiger-Shell-reset.sh
    Reformulando e simplificando a frase de informação.

--- Tiger-Shell-reset.sh
+++ Tiger-Shell-reset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yad --borders=32 --width=640 --button="Não, cancele":1 --button="Sim, resete a interface":0 --title="Resetar o Tiger Shell" --center \
-    --text="<big><b>Como deseja resetar o Tiger Shell?</b></big>\nNote que essa ação <b>NÃO</b> pode ser desfeita, seus arquivos não serão afetados\n"  && { 
+    --text="<big><b>Deseja resetar a interface do TigerOS?</b></big>\nNote que essa ação <b>NÃO</b> pode ser desfeita, mas seus arquivos não serão afetados\n"  && { 
         tiger-shell.sh --refresh;
 }
 
